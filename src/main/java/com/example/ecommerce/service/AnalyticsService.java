258c6a28a24f560a6015b1e559ee7dbbbef80b38
paramesh <parameshdarshan1234@gmail.com>
2025-07-28 23:53:31 +0530

    Service annote

--- src/main/java/com/example/ecommerce/service/AnalyticsService.java
+++ src/main/java/com/example/ecommerce/service/AnalyticsService.java
@@ -3,8 +3,7 @@ package com.example.ecommerce.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
-
-
+import org.springframework.stereotype.Service;
 
 import java.util.Map;
 import java.util.concurrent.*;
@@ -12,6 +11,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.StampedLock;
 import java.util.stream.Collectors;
 
+@Service
 public class AnalyticsService {
 
    // @Autowired
